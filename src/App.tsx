967165f1b2a7a1885706decc0c08002841757add
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-11-22 03:01:54 +0530

    login sign up toast

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
               <Navbar />
               <CartSidebar />
               <BookingButton />
+             
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
